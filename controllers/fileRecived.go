b33a8c82577a265d765a5c53b9de5a55a7cb7bf2
cgvalezuelac <elgael06@gmail.com>
2021-09-22 06:50:29 +0000

    leer archivo cvs.

--- controllers/fileRecived.go
+++ controllers/fileRecived.go
@@ -1,19 +1,30 @@
 package controllers
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func FileRecivedCVS(w http.ResponseWriter, r *http.Request) {
-	file, h, err := r.FormFile("cvs")
-	fmt.Println("h->", h)
-	fmt.Println("file->", file)
+	r.ParseMultipartForm(20000)
+	file, info, err := r.FormFile("cvs")
 	fmt.Println("err->", err)
 	if err != nil {
 		json.NewEncoder(w).Encode(err.Error())
 		return
 	}
-	json.NewEncoder(w).Encode("{message:'ok'}")
+	var buf bytes.Buffer
+	defer file.Close()
+	fmt.Println("name->", info.Filename)
+	fmt.Println("file->", file)
+
+	io.Copy(&buf, file)
+
+	contents := buf.String()
+	fmt.Println(contents)
+	buf.Reset()
+	json.NewEncoder(w).Encode(contents)
 }
